708e882bde49f42a200cbc2f5d54a34c8b467f17
Harsha <88052127+HarshaRteja@users.noreply.github.com>
2022-01-12 23:14:46 +0530

    createComponent card-list.component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React, { Component } from "react/cjs/react.production.min";
-
+import { CardList } from "./components/card-list/card-list.component";
 //class component method
 class App extends Component {
   constructor() {
@@ -21,6 +21,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <CardList name='Harsha'><h1>Harsha</h1></CardList>
         {this.state.monsters.map((monsters) => (
           <h1 key={monsters.id}>{monsters.name}</h1>
         ))}
