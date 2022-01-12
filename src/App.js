f4249d7b618c9821be8ecf6e3b3d01eab60a7b2a
Harsha <88052127+HarshaRteja@users.noreply.github.com>
2022-01-12 20:23:39 +0530

    componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import logo from "./logo.svg";
 import "./App.css";
 import React, { Component } from "react/cjs/react.production.min";
 
+//class component method
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: "Frankenstine",
-          id: "m1",
-        },
-        {
-          name: "Dracula",
-          id: "m2",
-        },
-        {
-          name: "Zombie",
-          id: "m3",
-        },
-      ],
+      monsters: [],
     };
   }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+      response.json()
+      .then(users => this.setState({monsters: users}))
+    );
+  }
+
   render() {
     return (
       <div className="App">
@@ -33,6 +28,7 @@ class App extends Component {
     );
   }
 }
+// Hook fucntions
 // function App() {
 //   return (
 
