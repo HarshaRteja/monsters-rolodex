e77a00b537d7020fc072f96d25a27355fdcd7f4d
Harsha <88052127+HarshaRteja@users.noreply.github.com>
2022-01-12 19:36:08 +0530

    setState

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import "./App.css";
 import { Component } from "react/cjs/react.production.min";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      String: "Hello, this is Harsha!",
+    };
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello this is Harsha!</p>
-          <button>
+          <p>{this.state.String}</p>
+          <button onClick={() => this.setState({ string: "Hello World!" })}>
             Change Text
           </button>
         </header>
